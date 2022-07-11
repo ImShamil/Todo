c0126226a4a682bd9aabdf55a9792f5acc1540fb
Imandinov_Shamil <flip0173@gmail.com>
2022-07-12 00:58:11 +0400

    test

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,17 +1,17 @@
-// import React from 'react';
 import Checkbox from '@mui/material/Checkbox';
 import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import PropTypes from 'prop-types';
 
-function ItemList() {
+function ItemList({ item }) {
   return (
     <Stack direction="row" alignItems="center" spacing={1}>
       <Checkbox inputProps={{ 'aria-label': 'Checkbox demo' }} />
       <TextField
         id="filled-size-normal"
-        defaultValue="Task"
+        defaultValue={item}
         variant="filled"
       />
       <IconButton aria-label="delete" size="large">
@@ -20,4 +20,7 @@ function ItemList() {
     </Stack>
   );
 }
+ItemList.propTypes = {
+  item: PropTypes.string.isRequired,
+};
 export default ItemList;
--- src/Components/List.js
+++ src/Components/List.js
@@ -1,25 +1,52 @@
-// import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
-import ItemList from './ItemList';
+import ItemList from './Item';
 
 function List() {
+  const [itemsList, setItemsList] = useState([]);
+  const [value, setValue] = useState('');
+
   const onChange = (e) => {
-    console.log(e.target.value);
+    setValue(e.target.value);
   };
+  const onSubmit = (e) => {
+    console.log(value);
+    if (value !== '') {
+      axios.post('http://localhost:3001/tasks', { task: value })
+        .then((res) => {
+          console.log(res);
+          console.log(res.data);
+        });
+      setValue('');
+    }
+    e.preventDefault();
+  };
+
+  useEffect(() => {
+    const apiUrl = 'http://localhost:3001/tasks';
+    axios.get(apiUrl).then((resp) => {
+      const tasks = resp.data;
+      setItemsList(tasks);
+    });
+  }, [setItemsList]);
+  console.log(itemsList);
 
   return (
     <Box>
-      <TextField
-        label="Task"
-        id="filled-size-normal"
-        defaultValue="Task"
-        variant="filled"
-        onChange={onChange}
-      />
-      <Button variant="outlined" color="success" size="large">Add</Button>
-      <ItemList />
+      <form onSubmit={onSubmit}>
+        <TextField
+          label="Task"
+          id="filled-size-normal"
+          defaultValue="Task"
+          variant="filled"
+          onChange={onChange}
+        />
+        <Button type="submit" variant="outlined" color="success" size="large">Add</Button>
+        {itemsList.map((item) => <ItemList item={item.task} />)}
+      </form>
     </Box>
   );
 }
