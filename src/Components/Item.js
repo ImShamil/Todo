8c2e7e760b276d85513080935ce04f9ed6bc3a16
Imandinov_Shamil <flip0173@gmail.com>
2022-07-12 17:02:47 +0400

    first version is done

--- src/Components/Item.js
+++ src/Components/Item.js
@@ -1,26 +1,76 @@
+/* eslint-disable no-unused-vars */
+import axios from 'axios';
 import Checkbox from '@mui/material/Checkbox';
-import TextField from '@mui/material/TextField';
 import Stack from '@mui/material/Stack';
 import IconButton from '@mui/material/IconButton';
 import DeleteIcon from '@mui/icons-material/Delete';
+import Button from '@mui/material/Button';
+import Input from '@mui/material/Input';
 import PropTypes from 'prop-types';
 
-function ItemList({ item }) {
+function ItemList({
+  index, task, itemsList, setItemsList,
+}) {
+  const copy = Object.assign([], itemsList);
+
+  const startEdit = () => {
+    copy[index].isEdit = true;
+    setItemsList(copy);
+  };
+
+  const endEdit = () => {
+    copy[index].isEdit = false;
+    axios.patch(`http://localhost:3001/tasks/${task.id}`, {
+      task: copy[index].task,
+    });
+    // .then((res) => {
+    //   console.log(res);
+    //   console.log(res.data);
+    // });
+    setItemsList(copy);
+  };
+
+  const changeNote = (e) => {
+    copy[index].task = e.target.value;
+  };
+
+  const deleteItem = (curIndex) => {
+    const newItemsList = itemsList.filter((item, itemIndex) => itemIndex !== curIndex);
+    axios.delete(`http://localhost:3001/tasks/${task.id}`, {
+      task: copy[index].task,
+    });
+    setItemsList(newItemsList);
+  };
+
+  let elem;
+
+  if (!task.isEdit) {
+    elem = (<Button onClick={startEdit} variant="text">{task.task}</Button>);
+  } else {
+    elem = (
+      <Input
+        onBlur={endEdit}
+        onChange={changeNote}
+      >
+        {task}
+      </Input>
+    );
+  }
+
   return (
     <Stack direction="row" alignItems="center" spacing={1}>
       <Checkbox inputProps={{ 'aria-label': 'Checkbox demo' }} />
-      <TextField
-        id="filled-size-normal"
-        defaultValue={item}
-        variant="filled"
-      />
-      <IconButton aria-label="delete" size="large">
+      {elem}
+      <IconButton aria-label="delete" size="large" onClick={() => deleteItem(index)}>
         <DeleteIcon />
       </IconButton>
     </Stack>
   );
 }
 ItemList.propTypes = {
-  item: PropTypes.string.isRequired,
+  index: PropTypes.number.isRequired,
+  task: PropTypes.shape.isRequired,
+  setItemsList: PropTypes.func.isRequired,
+  itemsList: PropTypes.shape.isRequired,
 };
 export default ItemList;
--- src/Components/List.js
+++ src/Components/List.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Box from '@mui/material/Box';
 import Button from '@mui/material/Button';
+import Paper from '@mui/material/Paper';
 import ItemList from './Item';
 
 function List() {
@@ -12,14 +13,18 @@ function List() {
   const onChange = (e) => {
     setValue(e.target.value);
   };
+
   const onSubmit = (e) => {
-    console.log(value);
     if (value !== '') {
-      axios.post('http://localhost:3001/tasks', { task: value })
-        .then((res) => {
-          console.log(res);
-          console.log(res.data);
-        });
+      axios.post('http://localhost:3001/tasks', {
+        id: itemsList.length + 1,
+        task: value,
+        isEdit: false,
+      });
+      // .then((res) => {
+      //   console.log(res);
+      //   console.log(res.data);
+      // });
       setValue('');
     }
     e.preventDefault();
@@ -31,23 +36,31 @@ function List() {
       const tasks = resp.data;
       setItemsList(tasks);
     });
-  }, [setItemsList]);
-  console.log(itemsList);
+  }, [value]);
 
   return (
-    <Box>
-      <form onSubmit={onSubmit}>
-        <TextField
-          label="Task"
-          id="filled-size-normal"
-          defaultValue="Task"
-          variant="filled"
-          onChange={onChange}
-        />
-        <Button type="submit" variant="outlined" color="success" size="large">Add</Button>
-        {itemsList.map((item) => <ItemList item={item.task} />)}
-      </form>
-    </Box>
+    <Paper variant="outlined">
+      <Box>
+        <form onSubmit={onSubmit}>
+          <TextField
+            label="Task"
+            id="filled-size-normal"
+            placeholder="Task"
+            variant="outlined"
+            onChange={onChange}
+          />
+          <Button type="submit" variant="outlined" color="success" size="large">Add</Button>
+          {itemsList.map((item, index) => (
+            <ItemList
+              index={index}
+              task={item}
+              itemsList={itemsList}
+              setItemsList={setItemsList}
+            />
+          ))}
+        </form>
+      </Box>
+    </Paper>
   );
 }
 export default List;
